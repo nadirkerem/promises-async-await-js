be404ee3d3a622a0c7c49c08e37bac0754b17b2d
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-17 17:36:43 -0400

    add promise chaining approach

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from './databases.js';
 
+// Using Promise
 function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
+
+  return central(id)
+    .then((db) => {
+      const database = dbs[db];
+      return Promise.all([database(id), vault(id)]);
+    })
+    .then(([basicInfo, personalInfo]) => ({
+      id,
+      ...basicInfo,
+      ...personalInfo,
+    }))
+    .catch(() => {
+      throw new Error('User not found');
+    });
 }
+
+getUserData(3).then(console.log).catch(console.error);
+getUserData(5).then(console.log).catch(console.error);
+getUserData(0).then(console.log).catch(console.error);
