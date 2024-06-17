2e83136752f9524adec4c9bac82284fccd65b3c7
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-17 17:50:37 -0400

    add async/await approach

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import { central, db1, db2, db3, vault } from './databases.js';
 
 // Using Promise
+/*
 function getUserData(id) {
   const dbs = {
     db1: db1,
@@ -27,3 +28,35 @@ function getUserData(id) {
 getUserData(3).then(console.log).catch(console.error);
 getUserData(5).then(console.log).catch(console.error);
 getUserData(0).then(console.log).catch(console.error);
+*/
+
+// Using async/await
+async function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+
+  try {
+    const databaseID = await central(id);
+    const database = dbs[databaseID];
+
+    const [basicInfo, personalInfo] = await Promise.all([
+      database(id),
+      vault(id),
+    ]);
+
+    return {
+      id,
+      ...basicInfo,
+      ...personalInfo,
+    };
+  } catch (error) {
+    return Promise.reject(`Database error: ${error}`);
+  }
+}
+
+getUserData(4).then(console.log).catch(console.error);
+getUserData(6).then(console.log).catch(console.error);
+getUserData(11).then(console.log).catch(console.error);
